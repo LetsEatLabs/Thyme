8f6ef174821b05e2b351a59c0f5d65360c18b608
turnipsoup <jeremy@jeremyheckt.com>
2023-03-07 20:20:49 -0800

    added help message, --help, and removed test.txt

--- main.go
+++ main.go
@@ -7,7 +7,21 @@ import (
 	"os"
 )
 
-// Read the prompts.json file and return it as a map///
+// Help message to display when the user asks for help or
+// fails to pass any arguments
+func helpMessage() {
+	helpStr := `
+Usage: chatgpt <flags> <input file>
+
+Flags:
+    -p <prompt>     The prompt to use for the chatbot
+    -h (--help)     Display this help message
+`
+
+	fmt.Println(helpStr)
+}
+
+// Read the prompts.json file and return it as a map
 func readPromptsConfig() map[string]interface{} {
 	var result map[string]interface{}
 	promptsConfigString := readFileToString("prompts.json")
@@ -23,7 +37,20 @@ func readPromptsConfig() map[string]interface{} {
 // Parses the command line arguments and returns them in a map
 // of flag:argument, if there are an uneven number of arguments then we
 // name the final argument "input" and add it to the map.
+// If the user asks for help or fails to pass any
+// Display the help message and exit.
 func parseArgs() map[string]string {
+
+	if len(os.Args) == 1 {
+		helpMessage()
+		os.Exit(0)
+	}
+
+	if os.Args[1] == "-h" || os.Args[1] == "--help" {
+		helpMessage()
+		os.Exit(0)
+	}
+
 	args := os.Args[1:]
 	result := make(map[string]string)
 	for i := 0; i < len(args); i += 2 {
