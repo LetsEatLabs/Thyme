0fe6abb34349aedd05b3f6478f8b9c9ae65f9600
turnipsoup <jeremy@jeremyheckt.com>
2023-03-24 20:21:40 -0700

    I think I finished the url summary function

--- kagiapi.go
+++ kagiapi.go
@@ -1,10 +1,30 @@
 package main
 
-import "os"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+var (
+	kagiURLSummaryEndpoint = "https://kagi.com/api/v0/summarize"
+	kagiKey                = os.Getenv("KAGI_API_KEY")
+	kagiAuthHeader         = fmt.Sprintf("Bot %s", kagiKey)
+)
 
 type KagiResponse struct {
-	Output string
-	Tokens int
+	Data struct {
+		Output string `json:"output"`
+		Tokens int    `json:"tokens"`
+	} `json:"data"`
+
+	Meta struct {
+		id   string `json:"id"`
+		node string `json:"node"`
+		ms   int    `json:"ms"`
+	} `json:"meta"`
 }
 
 type KagiRequest struct {
@@ -13,20 +33,69 @@ type KagiRequest struct {
 	Type   string // url, text, or file
 }
 
-func makeURLSummaryRequest(url string, kagi KagiRequest) KagiResponse {
-	kagiKey := os.Getenv("KAGI_API_KEY")
-}
+func makeURLSummaryRequest(kagi KagiRequest) KagiResponse {
 
-func makeTextSummaryRequest(url string, kagi KagiRequest) KagiResponse {
-	//kagiKey := os.Getenv("KAGI_API_KEY")
+	// Sanitize our input to make a JSON string
+	cleanInput, err := json.Marshal(kagi.Input)
+	if err != nil {
+		fmt.Println("Error marshalling input: ", err)
+		os.Exit(1)
+	}
 
-	response := KagiResponse{Output: "Not Implemented", Tokens: 0}
+	cleanEngine, err := json.Marshal(kagi.Engine)
+	if err != nil {
+		fmt.Println("Error marshalling engine: ", err)
+		os.Exit(1)
+	}
+
+	// Set custom headers
+	headers := map[string]string{
+		"Authorization": kagiAuthHeader,
+		"Content-Type":  "application/json",
+	}
+
+	request := fmt.Sprintf(`{"url": "%s", "engine": "%s"}`, cleanInput, cleanEngine)
+	brequest := []byte(request) // Bytes so we can send it over the wire
+
+	// Create a new request with custom headers and JSON payload
+	req, err := http.NewRequest("POST", kagiURLSummaryEndpoint, bytes.NewBuffer(brequest))
+	if err != nil {
+		panic(err)
+	}
+
+	// Apply the headers to the request
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	// Make the HTTP request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Convert response body to JSON
+	var response KagiResponse
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		panic(err)
+	}
+
 	return response
 }
 
-func makeFileSummaryRequest(url string, kagi KagiRequest) KagiResponse {
-	//kagiKey := os.Getenv("KAGI_API_KEY")
+// func makeTextSummaryRequest(kagi KagiRequest) KagiResponse {
+// 	//kagiKey := os.Getenv("KAGI_API_KEY")
 
-	response := KagiResponse{Output: "Not Implemented", Tokens: 0}
-	return response
-}
+// 	response := KagiResponse{Output: "Not Implemented", Tokens: 0}
+// 	return response
+// }
+
+// func makeFileSummaryRequest(kagi KagiRequest) KagiResponse {
+// 	//kagiKey := os.Getenv("KAGI_API_KEY")
+
+// 	response := KagiResponse{Output: "Not Implemented", Tokens: 0}
+// 	return response
+// }
