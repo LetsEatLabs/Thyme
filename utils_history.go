e925acb52638e06c2fea7e393c6cd7870567ba1c
turnipsoup <jeremy@jeremyheckt.com>
2023-04-01 15:02:32 -0700

    can return a list of history files, need to make it useful

---
+++ utils_history.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func viewHistoryQueries() {
+	// Get the history file
+	historyFiles := getHistoryFiles()
+
+	fmt.Println(historyFiles)
+
+}
+
+/////////////
+
+func getHistoryFiles() map[string][]string {
+
+	historyFiles := make(map[string][]string)
+	// Get the history file
+	kagiDir := os.Getenv("THYME_QUERY_KAGI_LOGGING_DIR")
+	openaiDir := os.Getenv("THYME_QUERY_LOGGING_DIR")
+
+	historyFiles["kagi"] = getFilesInDir(kagiDir)
+	historyFiles["openai"] = getFilesInDir(openaiDir)
+
+	return historyFiles
+}
+
+/////////////
+
+// Get a list of all files in a directory
+func getFilesInDir(dir string) []string {
+	filenames := []string{}
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, f := range files {
+		if !f.IsDir() {
+			filenames = append(filenames, filepath.Join(dir, f.Name()))
+		}
+	}
+
+	return filenames
+}
