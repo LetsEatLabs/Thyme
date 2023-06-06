bc631dd57f5c8b2e143850e9ecce786d33d0c51b
jheckt <jeremy@jeremyheckt.com>
2023-06-06 15:29:26 -0700

    can get a response from the fastgpt, need to get sources

--- src/api_kagi.go
+++ src/api_kagi.go
@@ -11,6 +11,7 @@ import (
 
 var (
 	kagiURLSummaryEndpoint = "https://kagi.com/api/v0/summarize"
+	kagiFastGPTEndpoint    = "https://kagi.com/api/v0/fastgpt"
 	kagiKey                = os.Getenv("KAGI_API_KEY")
 	kagiAuthHeader         = fmt.Sprintf("Bot %s", kagiKey)
 )
@@ -35,7 +36,7 @@ type KagiRequest struct {
 	SummaryType string // summary or notes (points)
 }
 
-func makeSummaryRequest(kagi KagiRequest) KagiResponse {
+func makeKagiRequest(kagi KagiRequest) KagiResponse {
 
 	// Sanitize our input to make a JSON string
 	cleanInput, err := json.Marshal(kagi.Input)
@@ -56,8 +57,8 @@ func makeSummaryRequest(kagi KagiRequest) KagiResponse {
 		"Content-Type":  "application/json",
 	}
 
-	// Get the summary type. By default anything but "notes" is a "summary"
-	if kagi.SummaryType != "notes" {
+	// Get the summary type. By default anything but "notes" or "fastgpt" is a "summary"
+	if kagi.SummaryType != "notes" && kagi.SummaryType != "fastgpt" {
 		kagi.SummaryType = "summary"
 	}
 
@@ -72,13 +73,29 @@ func makeSummaryRequest(kagi KagiRequest) KagiResponse {
 		os.Exit(1)
 	}
 
-	request := fmt.Sprintf(`{"%s": %s, "engine": %s, "summary_type": %s}`,
-		kagi.Type, cleanInput, cleanEngine, cleanSumType)
+	// Initialize the request string
+	var request string
+
+	if kagi.Type == "fastgpt" {
+		request = fmt.Sprintf(`{"query": %s, "web_search": true, "cache": true}`, cleanInput)
+	} else {
+		request = fmt.Sprintf(`{"%s": %s, "engine": %s, "summary_type": %s}`,
+			kagi.Type, cleanInput, cleanEngine, cleanSumType)
+	}
 
 	brequest := []byte(request) // Bytes so we can send it over the wire
 
 	// Create a new request with custom headers and JSON payload
-	req, err := http.NewRequest("POST", kagiURLSummaryEndpoint, bytes.NewBuffer(brequest))
+	var usingEndpoint string
+
+	if kagi.Type == "fastgpt" {
+		usingEndpoint = kagiFastGPTEndpoint
+	} else {
+		usingEndpoint = kagiURLSummaryEndpoint
+	}
+
+	fmt.Println("Using endpoint: ", usingEndpoint, " with request: ", string(brequest))
+	req, err := http.NewRequest("POST", usingEndpoint, bytes.NewBuffer(brequest))
 	if err != nil {
 		fmt.Println(err)
 	}
